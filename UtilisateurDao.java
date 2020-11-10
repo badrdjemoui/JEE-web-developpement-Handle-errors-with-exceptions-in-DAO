b76223b292c718a364a9ece19caac7aed0c312d2
djemoui <34734830+badrdjemoui@users.noreply.github.com>
2020-11-10 06:50:28 +0100

    Add files via upload

---
+++ UtilisateurDao.java
@@ -0,0 +1,10 @@
+package com.dao;
+
+import java.util.List;
+
+import com.beans.Utilisateur;
+
+public interface UtilisateurDao {
+    void ajouter( Utilisateur utilisateur ) throws DaoException;
+    List<Utilisateur> lister() throws DaoException;
+}
